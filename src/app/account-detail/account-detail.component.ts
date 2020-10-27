c949f8eca7595d86fc6864ea64969f7ccb0bfedc
Duc Bui <minhduc170385@gmail.com>
2020-10-26 19:18:32 -0700

    update code for dropdownlist and checkbox in edit user

--- src/app/account-detail/account-detail.component.ts
+++ src/app/account-detail/account-detail.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from './../service/auth.service';
 import { User } from './../user';
 //import { FormGroup, FormControl } from '@angular/forms';
 import {FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Console } from 'console';
 @Component({
   selector: 'account-detail',
   templateUrl: './account-detail.component.html',
@@ -11,14 +12,23 @@ import {FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular
 export class AccountDetailComponent implements OnInit {
   accountForm:FormGroup;  
   @Input() account: User; 
-  constructor(private authService: AuthService) { }
+  
+  Roles: any =['admin','user'];
+
+  constructor(private authService: AuthService) {
+    console.log("+++++++");
+   }
+  
+
   ngOnInit(): void {
     //console.log("Detail>>>" + this.account.id);
     this.accountForm = new FormGroup({
       username: new FormControl('',[ 
         Validators.required, 
         Validators.minLength(3)]),
-      email: new FormControl('', Validators.required)    
+      email: new FormControl('', Validators.required),
+      role: new FormControl('', Validators.required),
+      active: new FormControl(true,[])
     });
   }
 
@@ -28,6 +38,10 @@ export class AccountDetailComponent implements OnInit {
   get email(){
     return this.accountForm.get('email');    
   }
+  get role(){
+    return this.accountForm.get('role');    
+  }
+
   get formControls() { return this.accountForm.controls; }
 
 
